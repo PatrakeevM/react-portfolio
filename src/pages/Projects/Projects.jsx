6bd88d3d8a92dd9cb454a2c7863e701311132bcc
unknown <patrakeev.matvej@bk.ru>
2024-05-31 21:36:37 +0300

    add content-loader

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -3,17 +3,23 @@ import React from "react";
 import projects from "../../assets/projectList";
 import styles from "./Projects.module.scss";
 import Project from "../../components/Project/Project";
+import Skeleton from "../../components/Skeleton";
 
 const Projects = () => {
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const skeleton = [...new Array(3)].map((_, index) => (
+    <Skeleton key={index} />
+  ));
+  const items = projects.map((project, index) => (
+    <Project key={project.title} id={index} {...project} />
+  ));
+
   return (
     <main className="section">
       <div className="container">
-        <h2 className="title-1">Projects</h2>
-        <ul className={styles.projects}>
-          {projects.map((project, i) => (
-            <Project key={project.title} id={i} {...project} />
-          ))}
-        </ul>
+        <h2 className="title-1">Мои проекты</h2>
+        <ul className={styles.projects}>{isLoading ? skeleton : items}</ul>
       </div>
     </main>
   );
